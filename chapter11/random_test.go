831b61d94657f9a2312d9122c1b2d5114bb05d27
fengtuo58 <fengtuo58@qq.com>
2021-08-11 11:47:17 +0800

    chapter11

---
+++ chapter11/random_test.go
@@ -0,0 +1,52 @@
+package word
+
+import "fmt"
+import "reflect"
+import "time"
+import "testing"
+import "math/rand"
+import "unicode"
+
+
+// IsPalindrome reports whether s reads the same forward and backward. // (Our first attempt.)
+func IsPalindrome(s string) bool {
+	var letters []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000))
+		fmt.Println(reflect.TypeOf(r))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed : %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("Ispalindrome(%q) = false", p)
+		}
+	}
+}
+
+
